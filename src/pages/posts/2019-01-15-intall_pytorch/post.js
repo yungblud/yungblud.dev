b1776c78bef2e8a63cad6a23c92cdce209475bce
최동호 <killi8n@okcoinkr.com>
2019-01-15 18:38:03 +0900

    added about pytorch, conda, tensorflow especially with installing

---
+++ src/pages/posts/2019-01-15-intall_pytorch/post.js
@@ -0,0 +1,6 @@
+export default {
+  title: `Install PyTorch`,
+  tags: ["pytorch", "anaconda", "python"],
+  spoiler: "Let's learn how to install pytorch through anaconda",
+  getContent: () => import("./document.mdx")
+};
---
+++ src/pages/posts/2019-01-15-intall_tensorflow/post.js
@@ -0,0 +1,6 @@
+export default {
+  title: `Install Tensorflow`,
+  tags: ["tensorflow", "anaconda", "python"],
+  spoiler: "Let's learn how to install tensorflow through anaconda",
+  getContent: () => import("./document.mdx")
+};
