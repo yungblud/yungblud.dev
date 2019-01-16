d9db5fc3205b616a7106ab67c0b1b6322301c370
최동호 <killi8n@okcoinkr.com>
2019-01-16 12:30:51 +0900

    commit

--- src/siteMetadata.js
+++ src/siteMetadata.js
@@ -3,9 +3,8 @@ export default {
   // <title> tag.
   title: "develician.wtf",
 
-  author: "James K Nelson",
-  description:
-    "A clone of Dan Abramov's overreacted.io using create-react-app-mdx and Navi",
+  author: "develician",
+  description: "jailbreak & js & deep learning",
 
   // The number of posts to a page on the site index.
   indexPageSize: 10
