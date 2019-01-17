4dc5a0a432965b862e35454e4024291090ef74a0
최동호 <killi8n@okcoinkr.com>
2019-01-17 11:03:22 +0900

    commit

---
+++ src/pages/posts/2019-01-17-tensorflow_linear_regression/post.js
@@ -0,0 +1,6 @@
+export default {
+  title: `TensorFlow LinearRegression`,
+  tags: ["tensorflow", "linear regression"],
+  spoiler: "A Linear Regression Summary with Tensorflow",
+  getContent: () => import("./document.mdx")
+};
