c76a48e8ed3e54cac37562831c030a7bee49d992
Cillian <cillian@Cillianui-MacBookPro.local>
2019-01-27 23:21:45 +0900

    document

---
+++ src/pages/posts/2019-01-27-bring_me_the_horizon_new_album/post.js
@@ -0,0 +1,6 @@
+export default {
+  title: `Bring Me The Horizon's new album is out!`,
+  tags: ["bring me the horizon", "album", "review"],
+  spoiler: "Bring me the horizon album review",
+  getContent: () => import("./document.mdx")
+};
