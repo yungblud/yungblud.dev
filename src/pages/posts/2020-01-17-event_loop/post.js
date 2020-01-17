302a381a151efce4c6f9b92a23710ad109b1e4ca
Dongho Choi <kkiathorn@gmail.com>
2020-01-17 19:12:27 +0900

    added new article

---
+++ src/pages/posts/2020-01-17-event_loop/post.js
@@ -0,0 +1,6 @@
+export default {
+  title: `Javascript - Event Loop`,
+  tags: ["event loop", "javascript"],
+  spoiler: "자바스크립트 이벤트 루프에 대해 알아봅시다.",
+  getContent: () => import("./document.mdx")
+};
