61086f8aa10f3a580197934147fb192f559dc4c9
killi8n <kkiathorn@gmail.com>
2020-01-19 20:53:50 +0900

    added new writing

---
+++ src/pages/posts/2020-01-19-js_primitive_type/post.js
@@ -0,0 +1,6 @@
+export default {
+  title: `Javascript's Primitive Type`,
+  tags: ["Primitive Type", "javascript"],
+  spoiler: "Js Primitive Type",
+  getContent: () => import("./document.mdx")
+};
