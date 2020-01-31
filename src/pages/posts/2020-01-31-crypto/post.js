bb9079e885b3d90fbf1bc9cfea88ed84431a0417
Dongho Choi <kkiathorn@gmail.com>
2020-01-31 19:14:13 +0900

    new article

---
+++ src/pages/posts/2020-01-31-crypto/post.js
@@ -0,0 +1,6 @@
+export default {
+  title: `Crypto Module`,
+  tags: ["crypto", "javascript", "hash", "encrypt", "decrypt"],
+  spoiler: "javascript's encryption, decryption",
+  getContent: () => import("./document.mdx")
+};
