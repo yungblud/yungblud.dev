29c1a3d8dee90a173b0ea42f047d688c3c650bee
yungblud <yungblud@kakao.com>
2020-03-20 09:59:27 +0900

    added new post

---
+++ src/pages/posts/2020-03-20-debounce-vs-throttle/post.js
@@ -0,0 +1,6 @@
+export default {
+  title: "Debounce vs Throttle 무엇을 써야 할까?",
+  tags: ["js", "javascript", "lodash", "debounce", "thrrotle"],
+  spoiler: "어떤 상황에 무엇을 써야 할까?",
+  getContent: () => import("./document.mdx")
+};
