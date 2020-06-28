8773799222550a1d2253e3a0579a787ec7a897d1
yungblud <yungblud@kakao.com>
2020-06-29 00:22:57 +0900

    deep copy

---
+++ src/pages/posts/2020-03-29-deep-copy/post.js
@@ -0,0 +1,6 @@
+export default {
+  title: "JS Deep Copy vs Shallow Copy",
+  tags: ["js", "javascript", "deep copy", "shallow copy"],
+  spoiler: "Deep Copy and Shallow Copy",
+  getContent: () => import("./document.mdx")
+};
