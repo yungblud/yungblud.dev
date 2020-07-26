9286f05f9c68347ec2201c9dfb0bdcc22cac01ce
yungblud <yungblud@kakao.com>
2020-07-26 13:42:14 +0900

    트위터 embed 추가 & 번역기사

---
+++ src/pages/posts/2020-07-26-deep-Google, Facebook, Amazon, Apple hearing faces delay/post.js
@@ -0,0 +1,6 @@
+export default {
+  title: "Google, Facebook, Amazon, Apple hearing faces delay",
+  tags: ["BBC", "News"],
+  spoiler: "Google, Facebook, Amazon, Apple hearing faces delay",
+  getContent: () => import("./document.mdx")
+};
